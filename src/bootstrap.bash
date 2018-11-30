d28e397be4a6e0e85ac6042792e1959eb3d5e833
eaxelson <erik.axelson@helsinki.fi>
2018-11-30 11:00:22 +0200

    Use package 'hfst' until 'hfst-dev' is available at pypi.

--- src/bootstrap.bash
+++ src/bootstrap.bash
@@ -9,5 +9,5 @@ export GIT_COMMITTER_EMAIL=anon@localhost
 
 git clone https://github.com/eaxelson/hfst-notebooks.git
 
-pip3 install hfst-dev
-python3 -c "import hfst_dev as hfst"
+pip install hfst
+python3 -c "import hfst"
