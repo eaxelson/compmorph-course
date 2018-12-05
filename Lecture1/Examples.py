a20452616d4d635541a53573c616af272f4ce487
eaxelson <erik.axelson@helsinki.fi>
2018-12-05 16:08:42 +0200

    Fix code parts.

--- Lecture1/Examples.py
+++ Lecture1/Examples.py
@@ -1,62 +1,105 @@
-# Morphological analysis and generation with HFST python API.
+# # HFST - Helsinki Finite-State Technology
+#
+# The HFST toolkit is intended for processing natural language
+# morphologies. The toolkit is demonstrated by wide-coverage
+# implementations of a number of languages of varying morphological
+# complexity. HFST is written mainly in C++, but there is a Python interface
+# which is demonstrated on these notebooks.
+#
+# For installation of the HFST package for Python, see our [PyPI pages](https://pypi.org/project/hfst/).
+#
+# For more information about the interface, see our [Github wiki pages](https://github.com/hfst/python-hfst-4.0/wiki).
+#
+# This notebook page demonstrates morphological analysis and generation with HFST python API.
+#
+# ## 1. Using existing morphologies
+#
+# ### 1.1 Use standalone morphology packages (does not work yet)
+#
+# First import the morphology packages with:
+#
+# `pip import hfst-morphologies`
+#
+# start python and run:
 
-# A) Using existing morphologies
+from hfst_morphologies import english_bnc
 
-# 1. This would be easiest for the user (not yet implemented):
-#
-# pip import hfst-morphologies
-# # start python and run:
-#
-# from hfst_morphologies import english
-#
-# english.analyze('cat')
-# # cat[N]+N        10.203125
-# # cat[V]+V+INF    18.454102
-#
-# english.generate('cat[N]+N')
-# # cat     10.203125
-# # Cat     15.745117
-#
-# english.tokenize('This is a cat.')
-# # ('This', 'is', 'a', 'cat', '.')
+english_bnc.analyze('cat')
 
-# 2. At the moment, the easiest way is:
+# Expect the result:
 #
-# Download the morphology from "https://korp.csc.fi/download/hfst-morphologies/en/wn-bnc/hfst-en-morph-wn-bnc.zip",
-# extract it and start python:
+# ```
+# cat[N]+N        10.203125
+# cat[V]+V+INF    18.454102
+# ```
+
+english_bnc.generate('cat[N]+N')
+
+# Expect the result:
+#
+# ```
+# cat     10.203125
+# Cat     15.745117
+# ```
+
+english_bnc.tokenize('This is a cat.')
+
+# Expect the result:
+#
+# `('This', 'is', 'a', 'cat', '.')`
+
+# ### 1.2 Download the morphologies and process them with HFST
+#
+# For instance, download the English morphology from
+# [Kielipankki](https://korp.csc.fi/download/hfst-morphologies/en/wn-bnc/hfst-en-morph-wn-bnc.zip) (The Language Bank of Finland),
+# extract it and run python:
 
 from hfst_dev import HfstTransducer
 
 analyzer = HfstTransducer.read_from_file('hfst-en-morph-wn-bnc/english.hfst')
+analyzer.lookup_optimize()
 generator = HfstTransducer.read_from_file('hfst-en-morph-wn-bnc/hfst-english-installable/en-generation.hfst.ol')
 
 print(analyzer.lookup('cat', output='text'))
 
+# Output is:
+#
+# ```
 # cat     cat[N]+N        10.203125
 # cat     cat[V]+V+INF    18.454102
+# ```
 
 print(generator.lookup('cat[N]+N', output='text'))
 
+# Output is:
+#
+# ```
 # cat[N]+N        cat     10.203125
 # cat[N]+N        Cat     15.745117
+# ```
 
-# 3. Compiling the morphologies from source (TODO: the binaries are compiled with foma as hfst-xfst fails to produce the right result):
+# ### 1.3 Compile the morphologies from source
+#
+# (TODO: the binaries are compiled with foma as hfst-xfst fails to produce the right result, so this will not work...)
 
-#from hfst_dev import compile_xfst_script
+from hfst_dev import compile_xfst_script
 
-#compile_xfst_script('hfst-en-morph-wn-bnc/src/english.script')
-#generator = HfstTransducer.read_from_file('hfst-en-morph-wn-bnc/src/english.hfst')
-#analyzer = HfstTransducer(generator)
-#analyzer.invert()
-#analyzer.minimize()
+compile_xfst_script('hfst-en-morph-wn-bnc/src/english.script')
+analyzer = HfstTransducer.read_from_file('hfst-en-morph-wn-bnc/src/english.hfst')
+analyzer.lookup_optimize()
+generator = HfstTransducer(analyzer)
+generator.invert()
+generator.minimize()
 
-#english.att  english.script  exceptions.lex  lexicon.lex
+print(analyzer.lookup('cat', output='text'))
+print(generator.lookup('cat[N]+N', output='text'))
 
-
-
-# B) Generating your own morphologies:
-
-# 1. LexC formalism is the easiest way to handle this. It is explained in [ADD_LINK].
+# ## 2. Generating your own morphologies:
+#
+# HFST supports LexC, TwolC and XFST formalisms.
+#
+# A very simple example that generates plural, singular and possessive forms
+# for a couple of English lexemes using LexC:
 
 from hfst_dev import compile_lexc_script
 
@@ -129,58 +172,5 @@ END
 """
 )
 
-# generator.lookup_optimize()
+generator.lookup_optimize()
 print(generator.lookup('sky+N+Pl'))
-
-
-# 2. Use Xerox-style regular expressions.
-# They are explained in [ADD_LINK].
-# The input and output still need to be manually tokenized,
-# i.e. symbols must be separated by spaces.
-# Also note that some special symbols must be preceded by a percent sign
-# so that they will be interpreted literally.
-
-from hfst_dev import regex
-
-generator = regex("c a t %+N:0 %+Sg:0 %+Poss:'s")
-print(generator.lookup('cat+N+Sg+Poss'))
-
-# Also possible via cross-product (alignment of input and output tokens cannot be controlled)
-
-generator = regex("[c a t %+N %+Sg %+Poss] .x. [c a t ' s]")
-print(generator.lookup('cat+N+Sg+Poss'))
-
-
-# 
-
-# 3. Construct state by state and transition by transition.
-# Classes HfstIterableTransducer and HfstTransition can be used for this.
-# We also need the special symbol EPSILON.
-# This is not the easiest way to do this, but demonstrates the very low-level
-# functions that exist in the HFST API.
-
-from hfst_dev import HfstIterableTransducer, HfstTransition, EPSILON
-
-tr = HfstIterableTransducer()  # the transducer initially has one state, numbered zero, that is not final
-s = tr.add_state()             # state number one is created
-tr.add_transition(s-1, HfstTransition(s, 'c', 'c', 0))  # add transition from state 0 to 1 with input symbol c and output symbol c with no weight
-s = tr.add_state()             # state number two is created...
-tr.add_transition(s-1, HfstTransition(s, 'a', 'a', 0))
-s = tr.add_state()
-tr.add_transition(s-1, HfstTransition(s, 't', 't', 0))
-s = tr.add_state()
-tr.add_transition(s-1, HfstTransition(s, '+N', EPSILON, 0))
-s = tr.add_state()
-tr.add_transition(s-1, HfstTransition(s, '+Sg', EPSILON, 0))
-s = tr.add_state()
-tr.add_transition(s-1, HfstTransition(s, '+Poss', "'s", 0))
-tr.set_final_weight(s, 0)      # make the last state final with zero weight
-
-# Convert to HfstTransducer for lookup. HfstIterableTransducer is very limited and
-# mostly intended for constructing transducers from scratch and iterating through their
-# states and transitions.
-
-from hfst_dev import HfstTransducer
-
-generator = HfstTransducer(tr)
-print(generator.lookup('cat+N+Sg+Poss'))
