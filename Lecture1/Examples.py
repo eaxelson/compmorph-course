48ffbd2c972972f01dde5c10de8f764304f38f77
eaxelson <erik.axelson@helsinki.fi>
2018-12-05 10:20:55 +0200

    Rewrite the example.

--- Lecture1/Examples.py
+++ Lecture1/Examples.py
@@ -1,63 +1,62 @@
-# Morphological generator:
-# - input: cat+N+Sg+Poss
-# - output: cat's
+# Morphological analysis and generation with HFST python API.
 
-# Construct state by state and transition by transition.
-# Classes HfstIterableTransducer and HfstTransition can be used for this.
-# We also need the special symbol EPSILON.
-# This is not the easiest way to do this, but demonstrates the very low-level
-# functions that exist in the HFST API.
+# A) Using existing morphologies
 
-from hfst_dev import HfstIterableTransducer, HfstTransition, EPSILON
+# 1. This would be easiest for the user (not yet implemented):
+#
+# pip import hfst-morphologies
+# # start python and run:
+#
+# from hfst_morphologies import english
+#
+# english.analyze('cat')
+# # cat[N]+N        10.203125
+# # cat[V]+V+INF    18.454102
+#
+# english.generate('cat[N]+N')
+# # cat     10.203125
+# # Cat     15.745117
+#
+# english.tokenize('This is a cat.')
+# # ('This', 'is', 'a', 'cat', '.')
 
-tr = HfstIterableTransducer()  # the transducer initially has one state, numbered zero, that is not final
-s = tr.add_state()             # state number one is created
-tr.add_transition(s-1, HfstTransition(s, 'c', 'c', 0))  # add transition from state 0 to 1 with input symbol c and output symbol c with no weight
-s = tr.add_state()             # state number two is created...
-tr.add_transition(s-1, HfstTransition(s, 'a', 'a', 0))
-s = tr.add_state()
-tr.add_transition(s-1, HfstTransition(s, 't', 't', 0))
-s = tr.add_state()
-tr.add_transition(s-1, HfstTransition(s, '+N', EPSILON, 0))
-s = tr.add_state()
-tr.add_transition(s-1, HfstTransition(s, '+Sg', EPSILON, 0))
-s = tr.add_state()
-tr.add_transition(s-1, HfstTransition(s, '+Poss', "'s", 0))
-tr.set_final_weight(s, 0)      # make the last state final with zero weight
-
-# Convert to HfstTransducer for lookup. HfstIterableTransducer is very limited and
-# mostly intended for constructing transducers from scratch and iterating through their
-# states and transitions.
+# 2. At the moment, the easiest way is:
+#
+# Download the morphology from "https://korp.csc.fi/download/hfst-morphologies/en/wn-bnc/hfst-en-morph-wn-bnc.zip",
+# extract it and start python:
 
 from hfst_dev import HfstTransducer
 
-generator = HfstTransducer(tr)
-print(generator.lookup('cat+N+Sg+Poss'))
+analyzer = HfstTransducer.read_from_file('hfst-en-morph-wn-bnc/english.hfst')
+generator = HfstTransducer.read_from_file('hfst-en-morph-wn-bnc/hfst-english-installable/en-generation.hfst.ol')
 
-# Use Xerox-style regular expressions.
-# They are explained in [ADD_LINK].
-# The input and output still need to be manually tokenized,
-# i.e. symbols must be separated by spaces.
-# Also note that some special symbols must be preceded by a percent sign
-# so that they will be interpreted literally.
+print(analyzer.lookup('cat', output='text'))
 
-from hfst_dev import regex
+# cat     cat[N]+N        10.203125
+# cat     cat[V]+V+INF    18.454102
 
-generator = regex("c a t %+N:0 %+Sg:0 %+Poss:'s")
-print(generator.lookup('cat+N+Sg+Poss'))
+print(generator.lookup('cat[N]+N', output='text'))
 
-# Also possible via cross-product (alignment of input and output tokens cannot be controlled)
+# cat[N]+N        cat     10.203125
+# cat[N]+N        Cat     15.745117
 
-generator = regex("[c a t %+N %+Sg %+Poss] .x. [c a t ' s]")
-print(generator.lookup('cat+N+Sg+Poss'))
+# 3. Compiling the morphologies from source (TODO: the binaries are compiled with foma as hfst-xfst fails to produce the right result):
 
-# The regexp parser is a powerful tool, but for generating morphologies, there is an easier way.
+#from hfst_dev import compile_xfst_script
 
-# LexC formalism is the easiest way to handle this. It is explained in [ADD_LINK].
-# It requires some work to learn but
-# - multicharacter symbols need to declared only once in the beginning of the script
-# - no need to tokenize strings manually
-# - lexical entries and morphological processes are easy to separate in so called continuation lexica
+#compile_xfst_script('hfst-en-morph-wn-bnc/src/english.script')
+#generator = HfstTransducer.read_from_file('hfst-en-morph-wn-bnc/src/english.hfst')
+#analyzer = HfstTransducer(generator)
+#analyzer.invert()
+#analyzer.minimize()
+
+#english.att  english.script  exceptions.lex  lexicon.lex
+
+
+
+# B) Generating your own morphologies:
+
+# 1. LexC formalism is the easiest way to handle this. It is explained in [ADD_LINK].
 
 from hfst_dev import compile_lexc_script
 
@@ -132,3 +131,56 @@ END
 
 # generator.lookup_optimize()
 print(generator.lookup('sky+N+Pl'))
+
+
+# 2. Use Xerox-style regular expressions.
+# They are explained in [ADD_LINK].
+# The input and output still need to be manually tokenized,
+# i.e. symbols must be separated by spaces.
+# Also note that some special symbols must be preceded by a percent sign
+# so that they will be interpreted literally.
+
+from hfst_dev import regex
+
+generator = regex("c a t %+N:0 %+Sg:0 %+Poss:'s")
+print(generator.lookup('cat+N+Sg+Poss'))
+
+# Also possible via cross-product (alignment of input and output tokens cannot be controlled)
+
+generator = regex("[c a t %+N %+Sg %+Poss] .x. [c a t ' s]")
+print(generator.lookup('cat+N+Sg+Poss'))
+
+
+# 
+
+# 3. Construct state by state and transition by transition.
+# Classes HfstIterableTransducer and HfstTransition can be used for this.
+# We also need the special symbol EPSILON.
+# This is not the easiest way to do this, but demonstrates the very low-level
+# functions that exist in the HFST API.
+
+from hfst_dev import HfstIterableTransducer, HfstTransition, EPSILON
+
+tr = HfstIterableTransducer()  # the transducer initially has one state, numbered zero, that is not final
+s = tr.add_state()             # state number one is created
+tr.add_transition(s-1, HfstTransition(s, 'c', 'c', 0))  # add transition from state 0 to 1 with input symbol c and output symbol c with no weight
+s = tr.add_state()             # state number two is created...
+tr.add_transition(s-1, HfstTransition(s, 'a', 'a', 0))
+s = tr.add_state()
+tr.add_transition(s-1, HfstTransition(s, 't', 't', 0))
+s = tr.add_state()
+tr.add_transition(s-1, HfstTransition(s, '+N', EPSILON, 0))
+s = tr.add_state()
+tr.add_transition(s-1, HfstTransition(s, '+Sg', EPSILON, 0))
+s = tr.add_state()
+tr.add_transition(s-1, HfstTransition(s, '+Poss', "'s", 0))
+tr.set_final_weight(s, 0)      # make the last state final with zero weight
+
+# Convert to HfstTransducer for lookup. HfstIterableTransducer is very limited and
+# mostly intended for constructing transducers from scratch and iterating through their
+# states and transitions.
+
+from hfst_dev import HfstTransducer
+
+generator = HfstTransducer(tr)
+print(generator.lookup('cat+N+Sg+Poss'))
