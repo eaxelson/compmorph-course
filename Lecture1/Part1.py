b5ad3859e2cbb84cb798767328fed12d18b2f403
eaxelson <erik.axelson@helsinki.fi>
2018-12-12 10:20:00 +0200

    Add concatenative lexicon.

--- Lecture1/Part1.py
+++ Lecture1/Part1.py
@@ -50,11 +50,68 @@
 #  - E.g., sus- in all plural forms except nominative
 #  - No allomorph is more "basic" than any other.
 #
-# ## Item and Process (I&P)
+# ### Item and Process (I&P)
 #
 # We have roots or bases of morphemes and different processes apply to them:
 # - "SUTE"
 # - Nominative: word final 'e' becomes 'i'; 't' in front of 'i' becomes 's' => "susi"
 # - Genitive: add suffix '+n'; soften 't' to 'd' in closed syllable => "suden"
 # - Etc.
-
+#
+# ### Corresponding HFST tools
+#
+# | Model | HFST tools |
+# | - | - |
+# | Word & Paradigm | hfst-lexc, hfst-xfst |
+# | Item & Arrangement | hfst-lexc, hfst-xfst |
+# | Item & Processing | hfst-twolc, hfst-xfst |
+#
+# ## Morphological generators and analyzers
+#
+# ### Morphological generator
+#
+# * Input (also called lexical form): `cat+N+Sg+Poss`
+# * Output (also called surface form): `cat's`
+# * The idea is to create a model that generalizes to new word forms.
+#  - Wrong way: List all possible pairs of input and output in the lexeme:
+#   * `cat+N+Sg` -> cat
+#   * `cat+N+Pl` -> cats
+#   * `cat+N+Sg+Poss` -> cat's
+#   * `cat+N+Pl+Poss` -> cats'
+#  - Right way: Model the inner regular morphological structure of words.
+#   * This makes it possible to add a new lemma, such as `dog`, and the model knows how to inflect this word by analogy to the word `cat`.
+#
+# ### Morphological analyzer
+#
+# * Input (surface form): `cat's`
+# * Output (lexical form): `cat+N+Sg+Poss`
+# * An analyzer produces the opposite mapping compared to the generator:
+#  - The input of the generator is the output of the analyzer.
+#  - The output of the analyzer is the input of the generator.
+# * An analyzer is very useful, for instance:
+#  - when we want to parse natural language text syntactically
+#  - when we want to *normalize* text, such that we only care about the base form (lemma) of every word in the text; this is used, for instance, in *information retrieval*.
+#
+# ## Some simple noun paradigms in English
+#
+# Paradigm: N
+# cat +Sg (singular)
+# cat|s +Pl (plural)
+# cat|'s +Sg +Poss (singular possessive)
+# cat|s' +Pl +Poss (plural possessive)
+# Similarly: dog, pet, book, hill, fan
+#
+# Paradigm: N_s
+# kiss +Sg (singular)
+# kiss|es +Pl (plural)
+# kiss|'s +Sg +Poss (singular possessive)
+# kiss|es|' +Pl +Poss (plural possessive)
+# Similarly: wish, mess, church, search, waitress
+#
+# Let's create a morphological generator and analyzer for this data.
+#
+# ## 6. A Finite-State Transducer that implements a morphological generator
+#
+# A finite-state transducer for purely concatenative I&A English noun inflection
+# <img src="">
+# 
