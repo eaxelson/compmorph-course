e544d0a1c93521673d46d26bf1a1e739532fd64e
eaxelson <erik.axelson@helsinki.fi>
2018-12-17 13:49:23 +0200

    Write rest of lecture 2.

--- Lecture2/Part1.py
+++ Lecture2/Part1.py
@@ -140,7 +140,12 @@
 # `define InsertE   [. .] -> e || [ s | x | c h | s h | y ] %^ _ s ;`
 #
 # <img src="img/InsertE.png">
-#
+
+from hfst_dev import regex, HfstTransducer
+
+InsertE = regex("[. .] -> e || [ s | x | c h | s h | y ] %^ _ s")
+print(InsertE.lookup("sky^s'"))
+
 # ### Cascade of transducers: Rule 2
 #
 # Rewrite 'y' as 'i' when followed by the end of the stem, which is
@@ -151,7 +156,10 @@
 # `define YToI    y -> i || _ %^ e ;`
 #
 # <img src="img/YToI.png">
-#
+
+YToI = regex("y -> i || _ %^ e")
+print(YToI.lookup("sky^es'"))
+
 # ### Cascade of transducers: Rule 3
 #
 # Remove the end of stem marker
@@ -159,13 +167,22 @@
 # Expressed as an xfst rule:
 #
 # `define CleanUp    %^ -> 0 ;`
-#
+
+CleanUp = regex("%^ -> 0")
+print(CleanUp.lookup("ski^es'"))
+
 # <img src="img/CleanUp.png">
 #
 # ### Cascade equivalent to single FST
 #
 # <img src="img/cascade.png">
-#
+
+from hfst_dev import compile_lexc_file
+lexicon = compile_lexc_file('en_ia_morphology.lexc')
+
+from hfst_dev import compose
+cascade = compose((lexicon, InsertE, YToI, CleanUp))
+
 # When our lexicon is composed with our rules, we can actually produce one
 # single FST and 'jump' from the lexical-form input straight to the final
 # output in one go, without producing the intermediate steps.
@@ -179,12 +196,17 @@
 # ```
 #
 # The single FST will give directly: sky+N+Pl+Poss 🡒 skies.
-#
+
+print(cascade.lookup("sky+N+Pl+Poss"))
+
 # ### The order of the rules matters!
 #
 # What would happen if we reordered the rules (below) used in our simple
 # English noun morphology?
-#
+
+cascade = compose((lexicon,YToI, InsertE, CleanUp))
+print(cascade.lookup("sky+N+Pl+Poss"))
+
 # ### xfst notation explained in context
 #
 # <img src="img/xfst_notation_explained_1.png">
@@ -200,6 +222,8 @@
 # ## 4. Example: English adjectives
 #
 # ### Lexicon (lexc) of some English adjectives
+#
+# en_ip_adjectives_lexicon.lexc
 #
 # ```
 # Multichar_Symbols
@@ -240,26 +264,30 @@
 #
 # ### Suggested xfst script for English adjectives
 #
-# ```
-# ! Read lexicon and make a regex of it
-# read lexc en_ip_adjectives_lexicon.lexc
-# define Lexicon ;
-# regex Lexicon ;
-# 
-# ! y/i alternation
-# define YToI     y -> i || _ %^ e ;
-# 
-# ! Last rule cleans away the boundary marker
-# define CleanUp  %^ -> 0 ;
-# 
-# ! Compose lexicon with rules
-# regex Lexicon .o. YToI .o. CleanUp ;
-# 
-# ! Output all surface forms of the words
-# lower-words 
-# ```
-#
+
+from hfst_dev import compile_xfst_script
+
+adj_lexicon = compile_xfst_script("""
+! Read lexicon and make a regex of it
+read lexc en_ip_adjectives_lexicon.lexc
+define Lexicon ;
+regex Lexicon ;
+
+! y/i alternation
+define YToI     y -> i || _ %^ e ;
+
+! Last rule cleans away the boundary marker
+define CleanUp  %^ -> 0 ;
+
+! Compose lexicon with rules
+regex Lexicon .o. YToI .o. CleanUp ;
+
+! Output all surface forms of the words
+lower-words 
+""")
+
 # There are issues with some word forms...
+#
 # ```
 # big         biger       bigest
 # cool        cooler      coolest
@@ -279,35 +307,36 @@
 #
 # ### Corrected xfst script for English adjectives
 #
-# ```
-# ! Read lexicon and make a regex of it
-# read lexc en_ip_adjectives_lexicon.lexc
-# define Lexicon ;
-# regex Lexicon ;
-# 
-# define Vowel [ a | e | i | o | u | y ] ;
-# define Cons  [ b | c | d | f | g | h | j | k | l | m |
-# n | p | q | r | s | t | v | w | x | z ] ;
-# 
-# ! y/i alternation
-# define YToI     y -> i || _ %^ e ;
-# 
-# ! Consonant reduplication
-# define DoubleCons d -> d d ,
-# g -> g g ,
-# m -> m m ,
-# t -> t t || Cons Vowel _ %^ e ;
-# 
-# ! Last rule cleans away the boundary marker
-# define CleanUp  %^ -> 0 ;
-# 
-# ! Compose lexicon with rules
-# regex Lexicon .o. YToI .o. DoubleCons .o. CleanUp ;
-# 
-# ! Output all surface forms of the words
-# lower-words 
-# ```
-#
+
+adj_lexicon = compile_xfst_script("""
+! Read lexicon and make a regex of it
+read lexc en_ip_adjectives_lexicon.lexc
+define Lexicon ;
+regex Lexicon ;
+
+define Vowel [ a | e | i | o | u | y ] ;
+define Cons  [ b | c | d | f | g | h | j | k | l | m |
+n | p | q | r | s | t | v | w | x | z ] ;
+
+! y/i alternation
+define YToI     y -> i || _ %^ e ;
+
+! Consonant reduplication
+define DoubleCons d -> d d ,
+g -> g g ,
+m -> m m ,
+t -> t t || Cons Vowel _ %^ e ;
+
+! Last rule cleans away the boundary marker
+define CleanUp  %^ -> 0 ;
+
+! Compose lexicon with rules
+regex Lexicon .o. YToI .o. DoubleCons .o. CleanUp ;
+
+! Output all surface forms of the words
+lower-words 
+""")
+
 # Now it works!
 #
 # ```
