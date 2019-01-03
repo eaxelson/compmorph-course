6041dd2c0bbe0756011fc2128a1a807a7cdb5228
eaxelson <erik.axelson@helsinki.fi>
2019-01-03 14:06:12 +0200

    Use directory work.

--- src/bootstrap.bash
+++ src/bootstrap.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd /home/jovyan/
-
-# rm -r work
+cd /home/jovyan/work
 
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
@@ -10,4 +8,3 @@ export GIT_COMMITTER_EMAIL=anon@localhost
 git clone https://github.com/eaxelson/hfst-notebooks.git
 
 python3 -m pip install hfst-dev
-# python3 -c "import hfst_dev"
