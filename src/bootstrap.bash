7adef996c76e20ad2fabc16c228f8b51a6acee61
eaxelson <erik.axelson@helsinki.fi>
2019-01-03 13:34:03 +0200

    Do not import hfst_dev.

--- src/bootstrap.bash
+++ src/bootstrap.bash
@@ -2,7 +2,7 @@
 
 cd /home/jovyan/
 
-rm -r work
+# rm -r work
 
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
@@ -10,4 +10,4 @@ export GIT_COMMITTER_EMAIL=anon@localhost
 git clone https://github.com/eaxelson/hfst-notebooks.git
 
 python3 -m pip install hfst-dev
-python3 -c "import hfst_dev"
+# python3 -c "import hfst_dev"
