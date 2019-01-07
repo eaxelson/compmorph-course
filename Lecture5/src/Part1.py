17053988f3cee6f5e30b68c1a88fe6cf974f8cd3
eaxelson <erik.axelson@helsinki.fi>
2019-01-07 15:54:43 +0200

    Start adding a new lecture.

---
+++ Lecture5/src/Part1.py
@@ -0,0 +1,25 @@
+# # COMPUTATIONAL MORPHOLOGY WITH HFST TOOLS - LECTURE 5
+#
+# ## Section 1: Big picture
+#
+# ### Lecture 1: lexc
+#
+# "Lexicon without any replace rules"
+#
+# <img src="img/big_picture_lexc.png">
+#
+# ### Lectures 2 and 3: xfst and twolc
+#
+# "Lexicon combined with replace rules"
+#
+# <img src="img/big_picture_xfst_and_twolc.png">
+#
+# ### Lecture 4?: xfst / regular expressions 
+#
+# "Rules without much of a lexicon" 
+#
+# <img src="img/big_picture_xfst_and_regexps.png">
+#
+# ## Section 2: Guessers and stemmers
+#
+# 
