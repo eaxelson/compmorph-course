55bd78777ec9975df4c18b38ae5506425f8550a2
eaxelson <erik.axelson@helsinki.fi>
2019-01-15 11:34:37 +0200

    Add second chapter.

--- Lecture7/src/Part1.py
+++ Lecture7/src/Part1.py
@@ -141,10 +141,117 @@
 #
 # <img src="img/flag_diacritic_operators.png">
 #
-
-# <img src="img/foo.png">
+# ### P flag: positive (re)setting
 #
+# * Example:
+#   - `@P.CASE.GEN@`
+#   - Set the value of `CASE` to `GEN`
+#   - It does not matter what `CASE` was before, or if it was set at all
+#   - Never fails
+#
+# ### N flag: negative (re)setting
+#
+# * Example: `@N.CASE.GEN@`
+#   - Set the value of `CASE` to something else than `GEN`
+#   - The value of `CASE` is not well defined after this, but there is some value and we know it is not `GEN`
+#   - It does not matter what `CASE` was before, or if it was set at all
+#   - Never fails
+#
+# ### C flag: clear feature
+#
+# * Example: `@C.CASE@`
+#   - Unset the value of `CASE`
+#   - `CASE` has no value after this (also called neutral)
+#   - It does not matter what `CASE` was before, or if it was set at all
+#   - Never fails
+#
+# ### R flag: require test
+#
+# * Examples:
+#   - `@R.CASE.GEN@`
+#     - Succeeds if `CASE` has the value `GEN`
+#     - Otherwise fails and blocks this path
+#     - Does not set or modify the value of `CASE`
+#   - `@R.CASE@`
+#     - Succeeds if `CASE` is set to some value (not neutral)
+#     - Otherwise fails and blocks this path
+#     - Does not set or modify the value of `CASE`
+#
+# ### D flag: disallow test
+#
+# * Examples:
+#   - `@D.CASE.GEN@`
+#     - Succeeds if `CASE` does not have the value `GEN`
+#     - Otherwise fails and blocks this path
+#     - Does not set or modify the value of `CASE`
+#   - `@D.CASE@`
+#     - Succeeds if `CASE` is not set to any value (neutral)
+#     - Otherwise fails and blocks this path
+#     - Does not set or modify the value of `CASE`
+#
+# ### U flag: unification test
+#
+# * Examples:
+#   - `@U.CASE.GEN@`
+#     - Succeeds if `CASE` has the value `GEN`
+#     - Also succeeds if `CASE` is unset (neutral); in this case, `CASE` is set to `GEN` after this operation
+#     - Otherwise fails and blocks this path
+#   - `al@U.ART.PRESENT@kitaab@U.ART.ABSENT@uN@U.CASE.NOM@`
+#     - Fails
+#   - `bi@P.CASE.GEN@al@U.ART.PRESENT@kitaabi@U.CASE.GEN@`
+#     - Succeeds
+#
+# ### More examples with the same flag-diacritic operators
+#
+# <img src="img/n_foo_blah.png">
+#
+# * `@P.FEAT.M@` `@D.FEAT.M@` will fail.
+# * `@N.FEAT.M@` `@D.FEAT.Q@` will fail.
+# * `@R.FEAT@` will succeed if `FEAT` is currently set to some other value than neutral.
+# * `@U.FEAT.M@` succeeds if `FEAT` is currently set to `M` or neutral; the value is reset to `M` after this operation.
+# * `@N.FEAT.M@` `@U.FEAT.Q@` succeeds, leaving `FEAT=Q` in memory.
 
 # ## 2. Non-concatenative morphotactics
 #
-# 
+# ### Limited reduplication in Tagalog verbs
+#
+# <img src="img/tagalog_verbs.png">
+#
+# ### Full-stem reduplication in Malay nouns
+#
+# <img src="img/malay_nouns.png">
+#
+# ### Arabic word forms based on roots d-r-s and k-t-b
+#
+# <img src="img/arabic_word_forms.png">
+#
+# ### Tagalog limited reduplication: easy with two-level morphology!
+#
+# <img src="img/tagalog_reduplication.png">
+#
+# ### Malay full-stem reduplication with compile-replace
+#
+# <img src="img/malay_reduplication.png">
+#
+# ### Arabic “morphemes”
+#
+# <img src="img/arabic_morphemes.png">
+#
+# ### Arabic morphotactics using merge and compile-replace
+#
+# * xfst operator for merge to the right: `.m>.`
+# * xfst operator for merge to the left: `.<m.`
+# * For instance,
+#   - Lexical form: `[ ktb     +FormI     +Pass] +3P+Fem+Sg`
+#   - Surface form: `^[{ktb}.m>.{CVCVC}.<m.[u*i]^]at`
+# * The compile-replace algorithm executes the merge commands in the regular expression and produces the final surface form: `kutibat`
+#
+# ### Arabic morphotactics with Twol regular expression center rules
+#
+# <img src="img/arabic_morphotactics.png">
+#
+# ### More information
+#
+# * Chapter 7 of the Beesley & Karttunen book: “Flag Diacritics”
+# * Chapter 8 of the Beesley & Karttunen book: “Non-Concatenative Morphotactics”
+# * HFST: hfst-twolc − A Two-Level Grammar Compiler: TODO
