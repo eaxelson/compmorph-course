f07869137d8ead791a7fb349be46d5a06aea7497
eaxelson <erik.axelson@helsinki.fi>
2019-05-13 14:38:24 +0300

    Update repository name.

--- src/bootstrap.bash
+++ src/bootstrap.bash
@@ -5,7 +5,7 @@ cd /home/jovyan/work
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 
-git clone https://github.com/eaxelson/hfst-notebooks.git
+git clone https://github.com/eaxelson/compmorph-course.git
 
 python3 -m pip install graphviz
 python3 -m pip install hfst-dev
