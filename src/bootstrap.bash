544f97a4d6064612a5e22763d45ab658118a7ad0
eaxelson <erik.axelson@helsinki.fi>
2019-11-13 14:51:21 +0200

    Copy git repo to directory Lectures.

--- src/bootstrap.bash
+++ src/bootstrap.bash
@@ -6,6 +6,8 @@ export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 
 git clone https://github.com/eaxelson/compmorph-course.git
+cd /home/jovyan
+cp -r /home/jovyan/work/compmorph-course /home/jovyan/Lectures
 
 python3 -m pip install graphviz
 python3 -m pip install hfst-dev
